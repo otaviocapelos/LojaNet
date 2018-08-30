2d15629948f6c0dad05272ce254c63bf52fe8440
Otavio Capelos <otaviocapelos@yahoo.com.br>
2018-08-29 21:53:26 -0300

    Aula 8.1

---
+++ ExemploServico/ExemploServico/Service1.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ExemploServico
+{
+    public class Service1 : Servico01
+    {
+        public string Mensagem()
+        {
+            return "Isto vem de um servidor usando WCF";
+        }
+
+        public int Somar(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ExemploServico/ExemploServico/Servico01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ExemploServico
+{
+    [ServiceContract]
+    public interface Servico01
+    {
+        [OperationContract]
+        string Mensagem();
+        [OperationContract]
+        int Somar(int x, int y);
+
+    }
+}
